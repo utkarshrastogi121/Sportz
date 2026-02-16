8aa39829b59409ede782b173fdc00a7cab63ffd6
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2026-02-16 23:03:46 +0530

    initial backend commit

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app=express();
+const port=8000;
+
+app.use(express.json());
+app.get('/',(req,res)=>{
+    res.send('Hello from the backend')
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on port : ${port}`)
+})
